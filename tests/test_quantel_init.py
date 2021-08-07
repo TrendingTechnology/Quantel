310d1a5c7822d05ed55662c80a63cc870a300bf6
RatherBland <guy@ratherbland.com>
2021-08-07 22:58:11 +1000

    Updated test with correct env variable.

--- tests/test_quantel_init.py
+++ tests/test_quantel_init.py
@@ -4,7 +4,7 @@ from quantel import Quantel
 
 
 def test_init():
-    qt = Quantel(api_key=os.getenv("API_KEY"))
+    qt = Quantel(api_key=os.getenv("QUANTEL_API_KEY"))
     goog = qt.ticker('goog')
 
     assert goog.income_statement
