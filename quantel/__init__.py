3ad706357ba6dde77bed7d9c1bf89c8d7aa1e936
RatherBland <guy@ratherbland.com>
2021-08-14 18:07:07 +1000

    Added months arg to insider transactions**

--- quantel/__init__.py
+++ quantel/__init__.py
@@ -19,7 +19,7 @@ class _Ticker:
 
         self.symbols = list(self._chunks(symbols, 30))
 
-    def _get_data(self, endpoint) -> List[Dict]:
+    def _get_data(self, endpoint, **kwargs) -> List[Dict]:
 
         headers = {
             "x-rapidapi-key": self.api_key,
@@ -27,42 +27,42 @@ class _Ticker:
         }
 
         if self.asynchronous:
-            tasks = asyncio.get_event_loop().run_until_complete(self._submit_async(headers, endpoint))
+            tasks = asyncio.get_event_loop().run_until_complete(self._submit_async(headers, endpoint, **kwargs))
         else:
-            tasks = self._submit_sync(headers, endpoint)
+            tasks = self._submit_sync(headers, endpoint, **kwargs)
 
         result = filter(None, tasks)
         flat = [x for sublist in result for x in sublist]
         return flat
 
-    async def _submit_async(self, headers, endpoint):
+    async def _submit_async(self, headers, endpoint, **kwargs):
         async with aiohttp.ClientSession(headers=headers) as session:
             tasks = []
             for chunk in self.symbols:
                 joined_symbols = ",".join(chunk)
-                tasks.append(asyncio.ensure_future(self._get_data_async(session, endpoint, joined_symbols)))
+                tasks.append(asyncio.ensure_future(self._get_data_async(session, endpoint, joined_symbols, **kwargs)))
             res = await asyncio.gather(*tasks)
             return res
 
-    async def _get_data_async(self, session, endpoint, symbols):
-        async with session.get(f"{self.host}{endpoint}/{symbols}") as response:
+    async def _get_data_async(self, session, endpoint, symbols, **kwargs):
+        async with session.get(f"{self.host}{endpoint}/{symbols}", params=kwargs) as response:
             if response.status == 200:
                 return await response.json()
 
-    def _submit_sync(self, headers, endpoint):
+    def _submit_sync(self, headers, endpoint, **kwargs):
         tasks = []
 
         session = requests.Session()
         session.headers.update(headers)
         for chunk in self.symbols:
             joined_symbols = ",".join(chunk)
-            tasks.append(self._get_data_sync(session, endpoint, joined_symbols))
+            tasks.append(self._get_data_sync(session, endpoint, joined_symbols, **kwargs))
 
         return tasks
 
-    def _get_data_sync(self, session, endpoint, symbols):
+    def _get_data_sync(self, session, endpoint, symbols, **kwargs):
 
-        res = session.get(f"{self.host}{endpoint}/{symbols}")
+        res = session.get(f"{self.host}{endpoint}/{symbols}", params=kwargs)
 
         if res.status_code == 200:
             return res.json()
@@ -71,7 +71,6 @@ class _Ticker:
         n = max(1, n)
         return (l[i:i + n] for i in range(0, len(l), n))
 
-    @property
     def income_statement(self) -> List[Dict]:
         """
         Get income statements
@@ -81,7 +80,6 @@ class _Ticker:
         """
         return self._get_data("income-statement")
 
-    @property
     def income_statement_growth(self) -> List[Dict]:
         """
         Get income statements growth
@@ -90,7 +88,6 @@ class _Ticker:
         """
         return self._get_data("income-statement-growth")
 
-    @property
     def balance_sheet(self) -> List[Dict]:
         """
         Get balance sheets
@@ -99,7 +96,6 @@ class _Ticker:
         """
         return self._get_data("balance-sheet-statement")
 
-    @property
     def balance_sheet_growth(self) -> List[Dict]:
         """
         Get balance sheets growth
@@ -108,7 +104,6 @@ class _Ticker:
         """
         return self._get_data("balance-sheet-statement-growth")
 
-    @property
     def cash_flow(self) -> List[Dict]:
         """
         Get cash flow statements
@@ -117,7 +112,6 @@ class _Ticker:
         """
         return self._get_data("cash-flow-statement")
 
-    @property
     def cash_flow_growth(self) -> List[Dict]:
         """
         Get cash flow statements growth
@@ -126,7 +120,6 @@ class _Ticker:
         """
         return self._get_data("cash-flow-statement-growth")
 
-    @property
     def ratios(self) -> List[Dict]:
         """
         Get key financial ratios
@@ -135,7 +128,6 @@ class _Ticker:
         """
         return self._get_data("ratios")
 
-    @property
     def enterprise_values(self) -> List[Dict]:
         """
         Get enterprise values
@@ -144,7 +136,6 @@ class _Ticker:
         """
         return self._get_data("enterprise-values")
 
-    @property
     def key_metrics(self) -> List[Dict]:
         """
         Get key financial metrics
@@ -153,7 +144,6 @@ class _Ticker:
         """
         return self._get_data("key-metrics")
 
-    @property
     def analyst_estimates(self) -> List[Dict]:
         """
         Get analyst estimates of key financial figures
@@ -162,7 +152,6 @@ class _Ticker:
         """
         return self._get_data("analyst-estimates")
 
-    @property
     def shares_float(self) -> List[Dict]:
         """
         Get shares float and outstanding
@@ -171,7 +160,6 @@ class _Ticker:
         """
         return self._get_data("shares-float")
 
-    @property
     def quote(self) -> List[Dict]:
         """
         Get up to date quote
@@ -180,7 +168,6 @@ class _Ticker:
         """
         return self._get_data("quote")
 
-    @property
     def profile(self) -> List[Dict]:
         """
         Get company profile
@@ -189,8 +176,7 @@ class _Ticker:
         """
         return self._get_data("profile")
 
-    @property
-    def insider_transactions(self) -> List[Dict]:
+    def insider_transactions(self, months: int = None) -> List[Dict]:
         """
         Get all insider transactions
 
@@ -198,19 +184,17 @@ class _Ticker:
         """
 
         # TODO: Add months parameter.
-        return self._get_data("insider-transactions")
+        return self._get_data("insider-transactions", months=months)
 
-    @property
-    def insider_transactions_summarized(self) -> List[Dict]:
+    def insider_transactions_summarized(self, months: int = None) -> List[Dict]:
         """
         Get insider transactions summarized
 
         Returns:
         """
         # TODO: Add months parameter.
-        return self._get_data("insider-transactions-summarized")
+        return self._get_data("insider-transactions-summarized", months=months)
 
-    @property
     def share_ownership(self) -> List[Dict]:
         """
         Get share owning groups
@@ -219,7 +203,6 @@ class _Ticker:
         """
         return self._get_data("share-ownership")
 
-    @property
     def key_executives(self) -> List[Dict]:
         """
         Get key executives in org
--- tests/test_quantel_properties.py
+++ tests/test_quantel_properties.py
@@ -7,64 +7,64 @@ goog = qt.ticker('goog')
 
 
 def test_quantel_income_statement():
-    assert 'GOOG' in goog.income_statement[0]['symbol']
+    assert 'GOOG' in goog.income_statement()[0]['symbol']
 
 
 def test_quantel_income_statement_growth():
-    assert 'GOOG' in goog.income_statement_growth[0]['symbol']
+    assert 'GOOG' in goog.income_statement_growth()[0]['symbol']
 
 
 def test_quantel_balance_sheet():
-    assert 'GOOG' in goog.balance_sheet[0]['symbol']
+    assert 'GOOG' in goog.balance_sheet()[0]['symbol']
 
 
 def test_quantel_balance_sheet_growth():
-    assert 'GOOG' in goog.balance_sheet_growth[0]['symbol']
+    assert 'GOOG' in goog.balance_sheet_growth()[0]['symbol']
 
 
 def test_quantel_cash_flow():
-    assert 'GOOG' in goog.cash_flow[0]['symbol']
+    assert 'GOOG' in goog.cash_flow()[0]['symbol']
 
 
 def test_quantel_cash_flow_growth():
-    assert 'GOOG' in goog.cash_flow_growth[0]['symbol']
+    assert 'GOOG' in goog.cash_flow_growth()[0]['symbol']
 
 
 def test_quantel_ratios():
-    assert 'GOOG' in goog.ratios[0]['symbol']
+    assert 'GOOG' in goog.ratios()[0]['symbol']
 
 
 def test_quantel_enterprise_values():
-    assert 'GOOG' in goog.enterprise_values[0]['symbol']
+    assert 'GOOG' in goog.enterprise_values()[0]['symbol']
 
 
 def test_quantel_key_metrics():
-    assert 'GOOG' in goog.key_metrics[0]['symbol']
+    assert 'GOOG' in goog.key_metrics()[0]['symbol']
 
 
 def test_quantel_analyst_estimates():
-    assert 'GOOG' in goog.analyst_estimates[0]['symbol']
+    assert 'GOOG' in goog.analyst_estimates()[0]['symbol']
 
 
 def test_quantel_shares_float():
-    assert 'GOOG' in goog.shares_float[0]['symbol']
+    assert 'GOOG' in goog.shares_float()[0]['symbol']
 
 
 def test_quantel_quote():
-    assert 'GOOG' in goog.quote[0]['symbol']
+    assert 'GOOG' in goog.quote()[0]['symbol']
 
 
 def test_quantel_profile():
-    assert 'GOOG' in goog.profile[0]['symbol']
+    assert 'GOOG' in goog.profile()[0]['symbol']
 
 
 def test_quantel_insider_transactions():
-    assert 'GOOG' in goog.insider_transactions[0]['symbol']
+    assert 'GOOG' in goog.insider_transactions()[0]['symbol']
 
 
 def test_quantel_insider_transactions_summarized():
-    assert 'GOOG' in goog.insider_transactions_summarized[0]['symbol']
+    assert 'GOOG' in goog.insider_transactions_summarized()[0]['symbol']
 
 
 def test_quantel_share_ownership():
-    assert 'GOOG' in goog.share_ownership[0]['symbol']
+    assert 'GOOG' in goog.share_ownership()[0]['symbol']
